a97573aa8145a5b0b55fbca9c8690743299d79aa
Dave Hewitt <davidshewitt@posteo.net>
2021-05-17 10:20:51 -0700

    DO NOT MERGE: disable NTP

--- src/app.cpp
+++ src/app.cpp
@@ -98,11 +98,12 @@ void MultiballApp::begin(const char* app_name) {
 
 #define GMT_OFFSET_SECS  -8 * 60 * 60
 #define DAYLIGHT_SAVINGS_OFFSET_SECS 3600
-  configTime(GMT_OFFSET_SECS, DAYLIGHT_SAVINGS_OFFSET_SECS, "time.nist.gov", "0.pool.ntp.org", "1.pool.ntp.org");
-  struct tm timeinfo;
+//  configTime(GMT_OFFSET_SECS, DAYLIGHT_SAVINGS_OFFSET_SECS, "time.nist.gov", "0.pool.ntp.org", "1.pool.ntp.org");
+ // struct tm timeinfo;
 
   delay(300);
 
+/*
 #ifdef ESP32
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to obtain time");
@@ -112,6 +113,7 @@ void MultiballApp::begin(const char* app_name) {
   Serial.println("[ntp]");
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 #endif
+*/
 
 #ifdef ESP8266
   time_t now;
